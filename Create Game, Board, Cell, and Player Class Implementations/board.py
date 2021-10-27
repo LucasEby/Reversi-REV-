b341ef18a057215b0fd953c72a15c7d56cf327ff
sampontier <71413797+sampontier@users.noreply.github.com>
2021-10-27 10:34:11 -0400

    made suggested changes to Board

--- Create Game, Board, Cell, and Player Class Implementations/board.py
+++ Create Game, Board, Cell, and Player Class Implementations/board.py
@@ -2,14 +2,14 @@ from cell import *
 
 
 class Board:
-    def __init__(self, size: int, next_turn: int):
+    def __init__(self, size: int, next_turn: int) -> None:
         if size % 2 != 0:
             raise Exception("Board size must be an even number")
-        if (next_turn != 1) & (next_turn != 2):
+        if (next_turn != 1) and (next_turn != 2):
             raise Exception("Next turn must be a valid player (1 or 2)")
         self.size: int = size
         self.next_turn: int = next_turn
-        self.cells: Cell[size][size] = [
+        self.cells: List[Cell] = [
             [Cell.empty for c in range(size)] for r in range(size)
         ]
         # initialize the four starting disks at the center of the board
@@ -18,7 +18,7 @@ class Board:
         self.cells[size // 2 - 1][size // 2 - 1] = Cell.player2
         self.cells[size // 2][size // 2] = Cell.player2
 
-    def get_state(self):  # -> Cell[self.size][self.size]:
+    def get_state(self): -> List[Cell]:
         """
         Get the current state of the board, the cells field.
 
