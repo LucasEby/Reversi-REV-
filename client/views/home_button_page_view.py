d763fe317d47f9e8ae449534cb2a2dcce9735ae2
Ben Fox <b2k16@cinderhippo.com>
2021-10-28 20:27:39 -0400

    Ran black formatter

--- client/views/home_button_page_view.py
+++ client/views/home_button_page_view.py
@@ -2,11 +2,9 @@ from client.views.base_page_view import BasePageView
 
 
 class HomeButtonPageView(BasePageView):
-
     def display(self) -> None:
         """
         Displays home button to user
         TODO: Implement this with GUI
         """
         pass
-
