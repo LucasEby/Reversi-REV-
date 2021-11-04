a093398ccc31d54603433f29935579f23fe392d6
Aderland <wang.dail@northeastern.edu>
2021-11-04 08:11:56 -0400

    try integrating controller

--- User, Preferences, ManagePreferencesPageView Class Implementation/test_preference.py
+++
@@ -1,7 +0,0 @@
-from manage_preferences_page_controller import ManagePreferencesPageController
-from user import User
-
-
-user = User(21, "John")
-controller = ManagePreferencesPageController(user)
-controller.pref_go()
