50c92dd194a5f9c37f2cc0300d22c9337a313579
padraicjb <30627496+padraicjb@users.noreply.github.com>
2021-11-07 19:38:00 -0500

    actual implementation pretty much done

--- Abstract Rules and Standard Rules Implementation/standard_rules.py
+++ Abstract Rules and Standard Rules Implementation/standard_rules.py
@@ -1,5 +1,7 @@
 from abstract_rules import AbstractRules
 from typing import Tuple, List
+from client.model.board import Board
+
 
 
 class standard_rules(AbstractRules):
@@ -7,22 +9,22 @@ class standard_rules(AbstractRules):
         """
         finds the opposite player number given the current player number
         """
-        if( plyr_num == 1):
+        if(plyr_num == 1):
             return 2
-        if(plyr_num == 2):
+        elif(plyr_num == 2):
             return 1
         else:
             #throw an error
             pass
     #"""
-    def __check_horizontal_2(brd: Board, posn: Tuple[int, int],plyr_num: int):
+    def __check_horizontal(brd: Board, posn: Tuple[int, int],plyr_num: int):
         target_x = posn[0]
         target_y = posn[1]
         opp_num = __opponent_state(plyr_num)
-        if ((posn[0] > 1) and  (posn[0] < len(brd)-2)):
+        if ((target_x > 1) and  (target_x < len(brd)-2)):
             
             #Checking to the left here
-            if (posn[0] > 1):
+            if (target_x > 1):
                 
                 left_cells = []
                 #list that holds all the cells to the left of the target
@@ -49,72 +51,260 @@ class standard_rules(AbstractRules):
                             opp_pos = search_pos
                         #update the search position once searching is done
                         search_pos += 1
-                        #Check if an opponent exists to the left of the target and that a friendly exists to the right of that opponent
+                        #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
                         if(opp_pos > 0 and friend_pos > opp_pos):
                             return True
 
             #Checking to the right here
-            elif posn[0] < len(brd)-2:
-                pass
+            elif target_x < len(brd)-2:
+                right_cells = []
+                #list that holds all the cells to the right of the target
+                
+                #\/ Populate list of cells that are to the right of the target
+                for rght_cell in range(target_x, brd.len):
+                    right_cells = right_cells + brd[rght_cell, target_y]
+                
+                if (opp_num in right_cells ) and (plyr_num in right_cells):
+                    opp_pos = 0
+                    #nearest opponent cell position
+                    friend_pos = 0
+                    #nearest friendly cell position
+                    search_pos = 0
+                    #current position being checked
+                    for c in right_cells:
+                            #if the cell is a friendly cell update the friends cell position
+                            if c == plyr_num :
+                                friend_pos = search_pos
+                            #if the cell is an opponent cell update the opponent cell position
+                            elif c == opp_num:
+                                opp_pos = search_pos
+                            #update the search position once searching is done
+                            search_pos += 1
+                            #Check if an opponent exists to the right of the target and a friendly exists to the right of that opponent
+                            if(opp_pos > target_x and friend_pos > opp_pos):
+                                return True
         else:
             return False
-    #"""
 
-    def __check_horizontal(brd: Board, posn: Tuple[int, int],plyr_num: int):
-        is_horizontal_huh: bool = False
+    def __check_vertical(brd: Board, posn: Tuple[int, int], plyr_num: int):
+        target_x = posn[0]
+        target_y = posn[1]
+        opp_num = __opponent_state(plyr_num)
 
-        #cells: List[List[Cell]] list of list of cells, first list is the column, 2nd is the row
-        #  |0 1 2 3  Cells[2][1]
-        # 0|          posn[2][1]
-        # 1|? ? X
-        # 2|
-        # 3|
-
-        #check to the left
-        # iterate from 0 to posn[0] if posn[0] > 2
-        if(posn[0] > 1):
-            i = 0
-            for this_cell in brd.cells[posn[i]]:
-                #first we gotta check to see if their is even is an opponent cell to the left of the selected cell
-                if(cell.state == __opponent_state(plyr_num)):
-                    #since there is an opponent we then check for a friendly cell to the left of the opposing cell
-                    #for cell in brd.cells[???]:
-                    # if all of this is true we have 1 valid layout and can return true
-                    #return is_horizontal_huh, not sure which is better coding practice <- This or \/ that
-                    return True
-        
-        #check to the right
-        # position needs to be at least 3 away from the right edge of the board
-        if(posn[0] < brd.cells[0].length - 2):
-            #TODO: Do I have my rows and columns right?
-            for cell in range(posn[0],brd.cells[0].length):
-                #TODO: Does range work like I think it does?
-                if(cell.state == __opponent_state(plyr_num)):
-                    #there exists an opponent cell to the right of the current cell if we reach this line
-                    for cll in range(cell[0],brd.cells[0].length):
-                        #TODO: check range but needs to check to the right of the current cell from
-                        #      the previous for loop
-                        if(cll.state == plyr_num):
+        if ((target_y > 1) and  (target_y < len(brd)-2)):
+            
+            #Checking above here
+            if (target_y > 1):
+                
+                above_cells = []
+                #list that holds all the cells above the target
+                
+                #\/ Populate list of cells that are to the left of the target 
+                for abv_cll in range(0, target_y):
+                    above_cells = above_cells + brd[target_x, abv_cll]
+                
+                if (opp_num in above_cells ) and (plyr_num in above_cells):
+                    opp_pos = 0
+                    #nearest opponent cell position
+                    friend_pos = 0
+                    #nearest friendly cell position
+                    search_pos = 0
+                    #current position being checked
+                    
+                    #loop over the list of cells to left 
+                    for c in (above_cells):
+                        #if the cell is a friendly cell update the friends cell position
+                        if c == plyr_num :
+                            friend_pos = search_pos
+                        #if the cell is an opponent cell update the opponent cell position
+                        elif c == opp_num:
+                            opp_pos = search_pos
+                        #update the search position once searching is done
+                        search_pos += 1
+                        #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
+                        if(opp_pos < len(above_cells) and friend_pos < opp_pos):
                             return True
-        # wasnt a valid oreientation of tiles to the left or to the right
+            
+            #Checking below here
+            if (target_y < len(brd)-2):
+                
+                below_cells = []
+                #list that holds all the cells above the target
+                
+                #\/ Populate list of cells that are to the left of the target 
+                for blw_cll in range(target_y, len(brd)):
+                    below_cells = below_cells + brd[target_x, blw_cll]
+                
+                if (opp_num in below_cells ) and (plyr_num in below_cells):
+                    opp_pos = 0
+                    #nearest opponent cell position
+                    friend_pos = 0
+                    #nearest friendly cell position
+                    search_pos = 0
+                    #current position being checked
+                    
+                    #loop over the list of cells to left 
+                    for c in (below_cells):
+                        #if the cell is a friendly cell update the friends cell position
+                        if c == plyr_num :
+                            friend_pos = search_pos
+                        #if the cell is an opponent cell update the opponent cell position
+                        elif c == opp_num:
+                            opp_pos = search_pos
+                        #update the search position once searching is done
+                        search_pos += 1
+                        #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
+                        if(opp_pos > target_y and friend_pos > opp_pos):
+                            return True
+
         return False
     
-    def __check_vertical(brd: Board, posn: Tuple[int, int], plyr_num: int):
-        is_vertical_huh:bool = False
-
-        #check above
-        #check below
-
-        return is_vertical_huh
-    
     def __check_diagonal(brd: Board, posn: Tuple[int, int], plyr_num: int):
-        is_diagonal_huh:bool = False
+        
         #check up and left, increase row, decrease column
-        #check down and left, decrease row, decrease column
-        #check up and right, increase column, increase row
-        #check down and right, increase column, decrease row
-        return is_diagonal_huh
+        target_x: int = posn[0]
+        target_y = posn[1]
+        opp_num = __opponent_state(plyr_num)
 
+        #Check Up-Right
+        if ((target_x < len(brd)-2) and (target_y > 1)):
+            #not too close to the top right corner
+            up_right_cells = []
+            #list that holds all the cells up and to the right of the target
+            
+            #\/ Populate list of cells that are to the left of the target 
+            dim = [len (brd) - target_x, target_y]
+            smallest_dim = min(dim)
+            #need to ensure we dont get an index out of bounds error
+            for i in range(smallest_dim):
+                if i!=0:
+                    up_right_cells = up_right_cells + brd[target_x + i, target_y - i]
+    
+            if (opp_num in up_right_cells ) and (plyr_num in up_right_cells):
+                opp_pos = 0
+                #nearest opponent cell position
+                friend_pos = 0
+                #nearest friendly cell position
+                search_pos = 0
+                #current position being checked
+                
+                #loop over the list of cells to left 
+                for c in (up_right_cells):
+                    #if the cell is a friendly cell update the friends cell position
+                    if c == plyr_num :
+                        friend_pos = search_pos
+                    #if the cell is an opponent cell update the opponent cell position
+                    elif c == opp_num:
+                        opp_pos = search_pos
+                    #update the search position once searching is done
+                    search_pos += 1
+                    #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
+                    if(opp_pos > 0 and friend_pos > opp_pos):
+                        return True
+        
+        #Check Down-Left
+        if((target_x > 1) and  (target_y < len(brd)-2)):
+            #not too close to the bottom left corner
+            bot_left_cells = []
+            #list that holds all the cells down and to the left of the target
+            dim = [target_x, len (brd) - target_y]
+            smallest_dim = min(dim)
+            #need to ensure we dont get an index out of bounds error
+            for i in range(smallest_dim):
+                if i!=0:
+                    bot_left_cells = bot_left_cells + brd[target_x - i, target_y + i]
+            
+            if (opp_num in bot_left_cells ) and (plyr_num in bot_left_cells):
+                opp_pos = 0
+                #nearest opponent cell position
+                friend_pos = 0
+                #nearest friendly cell position
+                search_pos = 0
+                #current position being checked
+                
+                #loop over the list of cells to left 
+                for c in (bot_left_cells):
+                    #if the cell is a friendly cell update the friends cell position
+                    if c == plyr_num :
+                        friend_pos = search_pos
+                    #if the cell is an opponent cell update the opponent cell position
+                    elif c == opp_num:
+                        opp_pos = search_pos
+                    #update the search position once searching is done
+                    search_pos += 1
+                    #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
+                    if(opp_pos > 0 and friend_pos > opp_pos):
+                        return True
+        
+        #Check Up-Left
+        if((target_x > 1) and  (target_y > 1)):
+            #not too close to the top left corner
+            top_left_cells = []
+            #list that holds all the cells down and to the left of the target
+            dim = [target_x, target_y]
+            smallest_dim = min(dim)
+            #need to ensure we dont get an index out of bounds error
+            for i in range(smallest_dim):
+                if i!=0:
+                    top_left_cells = top_left_cells + brd[target_x - i, target_y - i]
+            
+            if (opp_num in top_left_cells ) and (plyr_num in top_left_cells):
+                opp_pos = 0
+                #nearest opponent cell position
+                friend_pos = 0
+                #nearest friendly cell position
+                search_pos = 0
+                #current position being checked
+                
+                #loop over the list of cells to left 
+                for c in (top_left_cells):
+                    #if the cell is a friendly cell update the friends cell position
+                    if c == plyr_num :
+                        friend_pos = search_pos
+                    #if the cell is an opponent cell update the opponent cell position
+                    elif c == opp_num:
+                        opp_pos = search_pos
+                    #update the search position once searching is done
+                    search_pos += 1
+                    #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
+                    if(opp_pos > 0 and friend_pos > opp_pos):
+                        return True
+        
+        #Check Down-Right
+        if((target_x < len(brd)-2)) and  (target_y < len(brd) -2 ):
+            #not too close to the top left corner
+            bot_right_cells = []
+            #list that holds all the cells down and to the left of the target
+            dim = [target_x, target_y]
+            smallest_dim = min(dim)
+            #need to ensure we dont get an index out of bounds error
+            for i in range(smallest_dim):
+                if i!=0:
+                    bot_right_cells = bot_right_cells + brd[target_x + i, target_y + i]
+            
+            if (opp_num in bot_right_cells ) and (plyr_num in bot_right_cells):
+                opp_pos = 0
+                #nearest opponent cell position
+                friend_pos = 0
+                #nearest friendly cell position
+                search_pos = 0
+                #current position being checked
+                
+                #loop over the list of cells to left 
+                for c in (bot_right_cells):
+                    #if the cell is a friendly cell update the friends cell position
+                    if c == plyr_num :
+                        friend_pos = search_pos
+                    #if the cell is an opponent cell update the opponent cell position
+                    elif c == opp_num:
+                        opp_pos = search_pos
+                    #update the search position once searching is done
+                    search_pos += 1
+                    #Check if an opponent exists to the left of the target and that a friendly exists to the left of that opponent
+                    if(opp_pos > 0 and friend_pos > opp_pos):
+                        return True
+
+        return False
     def __is_valid_move(plyr_num: int, posn: Tuple[int, int], brd: Board):
         """
         determines if a move that was played is valid based on a given ruleset
