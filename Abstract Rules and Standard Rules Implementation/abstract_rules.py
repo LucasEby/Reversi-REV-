84ce3414ef7e2c4f222c0d7803a2842186ee7548
padraicjb <30627496+padraicjb@users.noreply.github.com>
2021-11-10 14:22:45 -0500

    Addressed changed from git hub and reran black

--- Abstract Rules and Standard Rules Implementation/abstract_rules.py
+++ Abstract Rules and Standard Rules Implementation/abstract_rules.py
@@ -1,6 +1,4 @@
 from abc import ABC, abstractmethod
-
-# from client.model.player import Player
 from client.model.board import Board
 from typing import Tuple, List
 import string
@@ -16,13 +14,13 @@ class AbstractRules(ABC):
         :return: None
         """
         self.name: string = name_string
-        pass
 
+    @abstractmethod
     def __is_valid_move(plyr_num: int, posn: Tuple[int, int], brd: Board) -> bool:
         """
         determines if a move that was played is valid based on a given ruleset
 
-        :param ply_num:the number of players in the game
+        :param ply_num: the player number of the player attempting to place a disk
         :param posn: the position of the move that is being evaluated
         :param brd: current board state
         :raises error: None
