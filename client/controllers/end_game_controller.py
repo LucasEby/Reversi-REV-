286b996ba9423f5496cfa74a9fefeadf6f370176
padraicjb <30627496+padraicjb@users.noreply.github.com>
2021-11-13 00:06:48 -0500

    first stab at getting the controller and view with tkinter working properly

--- client/controllers/end_game_controller.py
+++ client/controllers/end_game_controller.py
@@ -3,11 +3,14 @@ from typing import Callable
 from client.controllers.base_page_controller import BasePageController
 from client.views.end_game_page_view import EndGamePageView
 
+
 class EndGameController(BasePageController):
-    def __init__(self, go_home_callback: Callable[[], None],
-    rematch_callback: Callable[[], None],
-    play_again_callback: Callable[[], None],
-    play_different_mode_callback: Callable[[], None]
+    def __init__(
+        self,
+        go_home_callback: Callable[[], None],
+        rematch_callback: Callable[[], None],
+        play_again_callback: Callable[[], None],
+        play_different_mode_callback: Callable[[], None],
     ) -> None:
         """
         Specific type of page controller with a home button already handled.
@@ -17,28 +20,34 @@ class EndGameController(BasePageController):
         super().__init__()
         self._rematch_callback: Callable[[], None] = rematch_callback
         self._play_again_callback: Callable[[], None] = play_again_callback
-        self._play_different_mode_callback: Callable[[], None] = play_different_mode_callback
+        self._play_different_mode_callback: Callable[
+            [], None
+        ] = play_different_mode_callback
 
         self._task_execute_dict["rematch_button"] = self.__execute_task_rematch_button
-        self._task_execute_dict["play_again_button"] = self.__execute_task_play_again_button
-        self._task_execute_dict["play_different_mode_button"] = self.__execute_task_play_different_mode_button
+        self._task_execute_dict[
+            "play_again_button"
+        ] = self.__execute_task_play_again_button
+        self._task_execute_dict[
+            "play_different_mode_button"
+        ] = self.__execute_task_play_different_mode_button
         self._view = EndGamePageView()
 
     def __handle_rematch_button(self) -> None:
         """
         Handles rematch button action from the user by queueing task
         """
-        self.queue(task_name = "rematch_button")
-    
+        self.queue(task_name="rematch_button")
+
     def __execute_task_rematch_button(self) -> None:
         self._rematch_callback()
-        
+
     def __handle_play_again(self) -> None:
         """
         Handles play again button action from the user by queueing task
         """
-        self.queue(task_name = "play_again")
-        
+        self.queue(task_name="play_again")
+
     def __execute_task_play_again_button(self) -> None:
         """
         Handles rematch button action from the user by queueing task
@@ -49,8 +58,8 @@ class EndGameController(BasePageController):
         """
         Handles play different mode button action from the user by queueing task
         """
-        self.queue(task_name = "play_different_mode_button")
-    
+        self.queue(task_name="play_different_mode_button")
+
     def __execute_task_play_different_mode_button(self) -> None:
         """
         Handles rematch button action from the user by queueing task
--- client/views/end_game_page_view.py
+++ client/views/end_game_page_view.py
@@ -11,28 +11,83 @@ class EndGamePageView(BasePageView):
 
     __ABC_ARRAY = list(string.ascii_lowercase)
 
-    def __init__(self, game_obj: Game,
-       # rematch_cb: Callable[],
-       # play_again_cb: Callable[],
+    def __init__(
+        self,
+        game_obj: Game,
+        rematch_cb: Callable[[int], None],
+        play_again_cb: Callable[[int], None],
         play_different_mode_cb: Callable[[int], None],
     ) -> None:
         """
-        
+
         :param game_obj: A specific game object who's attributes will be accessed and displayed.
-        :param place_tile_cb: Callback function to call when a tile is placed
-        :param forfeit_cb: Callback function to call when a player forfeits
+        :param rematch_cb: Callback function for playing the same game mode against the same opponent
+        :param play_again_cb: Callback function for playing the same mode again
+        :param play_different_mode_cb: Callback function for changing the game mode
         """
         self._game_obj: Game = game_obj
+        self._rematch_callback = rematch_cb
+        self._play_again_callback = play_again_cb
+        self._play_different_mode_callback = play_different_mode_cb
         super().__init__()
 
-        
-    def handle_rematch_callback():
-        pass
+    def display(self) -> None:
+        """
+        draws the End Game windows and its 5 elements
+        """
+        window = tk.Tk()
+        self.__score_label().pack()
+        self.__winner_label().pack()
+        self.__play_again_button().pack()
+        self.__rematch_button().pack()
+        self.__play_different_mode_button().pack()
 
+        window.mainloop()
 
-    def display_winner(self) -> None:
+    def __rematch_button(self) -> tk.Button:
+        """
+        Builds the rematch button
+        """
+        return tk.Button(
+            text="Rematch",
+            width=25,
+            height=5,
+            # bg="blue",fg="yellow",
+            command=self._rematch_callback,
+        )
+
+    def __play_again_button(self) -> tk.Button:
+        """
+        Builds the play again button
+        """
+        return tk.Button(
+            text="Play Again",
+            width=25,
+            height=5,
+            # bg="blue",fg="yellow",
+            command=self._play_again_callback,
+        )
+
+    def __play_different_mode_button(self) -> tk.Button:
+        """
+        Builds the play different mode button
+        """
+        return tk.Button(
+            text="Play Diiffernt Mode",
+            width=25,
+            height=5,
+            # bg="blue",fg="yellow",
+            command=self._play_different_mode_callback,
+        )
+
+    def __score_label(self) -> tk.Label:
+        """
+        Prints out the game's score.
+        """
+        return tk.Label(str(self._game_obj.get_score()))
+
+    def __winner_label(self) -> tk.Label:
         """
         Prints out the winner of the game.
         """
-        winner_string: string = str(self._game_obj.get_winner())
-        
+        return tk.Label(str(self._game_obj.get_score()))
