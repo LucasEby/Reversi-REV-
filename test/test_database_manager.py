365e3cb67ff9fe7a6ec09532733b141d01b5c77c
Ben Fox <b2k16@cinderhippo.com>
2021-11-14 22:02:04 -0500

    [REV-53] Database Manager

---
+++ test/test_database_manager.py
@@ -0,0 +1,255 @@
+import datetime
+import unittest
+from unittest.mock import MagicMock
+
+from server.database_management.database_manager import (
+    DatabaseManager,
+    DatabaseAccount,
+    DatabaseGame,
+)
+
+
+class TestDatabaseManager(unittest.TestCase):
+    def test_connection(self):
+        # Create, connect and disconnect database with no errors
+        dbm = DatabaseManager().get_instance()
+        dbm.connect_database()
+        dbm.disconnect_database()
+        # Ensure execution of query no longer works after disconnect
+        with self.assertRaises(Exception):
+            dbm._db_cursor.execute("use reversi")
+
+    def test_account(self):
+        # Connect to database
+        dbm = DatabaseManager().get_instance()
+        dbm.connect_database()
+
+        # Create account function with no errors, ensuring callback was called successfully
+        create_account_request_data = DatabaseAccount(
+            None,
+            "username",
+            "password",
+            0,
+            8,
+            "board_color",
+            "disk_color",
+            "opp_disk_color",
+            "line_color",
+            "rules",
+            False,
+        )
+        callback = MagicMock()
+        dbm.create_account(callback, create_account_request_data)
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        # Get account info and test it is all correct
+        get_account_request_data = (
+            False,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+        )
+        callback.reset_mock()
+        dbm.get_account(callback, "username", *get_account_request_data)
+        dbm.run()
+        callback.assert_called_once_with(
+            True, DatabaseAccount(*create_account_request_data)
+        )
+
+        # Get account ID
+        callback.reset_mock()
+        dbm.get_account(callback=callback, key="username", get_account_id=True)
+        dbm.run()
+        account_id: int = callback.call_args[0][1].account_id
+
+        # Get account info via account ID and verify it is all correct
+        callback.reset_mock()
+        dbm.get_account(callback, account_id, *get_account_request_data)
+        dbm.run()
+        callback.assert_called_once_with(
+            True, DatabaseAccount(*create_account_request_data)
+        )
+
+        # Update an account field and test it was updated
+        callback.reset_mock()
+        dbm.update_account(
+            callback=callback,
+            account_id=account_id,
+            database_account=DatabaseAccount(
+                password="password2", pref_rules="weird_rules"
+            ),
+        )
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        callback.reset_mock()
+        dbm.get_account(
+            callback=callback,
+            key=account_id,
+            get_username=True,
+            get_password=True,
+            get_pref_rules=True,
+        )
+        dbm.run()
+        dba: DatabaseAccount = callback.call_args[0][1]
+        self.assertEqual("username", dba.username)
+        self.assertEqual("password2", dba.password)
+        self.assertEqual("weird_rules", dba.pref_rules)
+
+        # Delete account and ensure it can no longer be retrieved
+        callback.reset_mock()
+        dbm.delete_account(callback=callback, account_id=account_id)
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        callback.reset_mock()
+        dbm.get_account(callback=callback, key=account_id, get_account_id=True)
+        dbm.run()
+        callback.assert_called_once_with(False, DatabaseAccount())
+
+        # Disconnect from database
+        dbm.disconnect_database()
+
+    def test_game(self):
+        # Connect to database
+        dbm = DatabaseManager().get_instance()
+        dbm.connect_database()
+
+        # Create account and get account ID so new game can reference it
+        create_account_request_data = DatabaseAccount(
+            None,
+            "username",
+            "password",
+            0,
+            8,
+            "board_color",
+            "disk_color",
+            "opp_disk_color",
+            "line_color",
+            "rules",
+            False,
+        )
+        callback = MagicMock()
+        dbm.create_account(callback, create_account_request_data)
+        dbm.run()
+        callback.assert_called_once_with(True)
+        callback.reset_mock()
+        dbm.get_account(callback=callback, key="username", get_account_id=True)
+        dbm.run()
+        account_id: int = callback.call_args[0][1].account_id
+
+        # Create game function with no errors
+        dbg: DatabaseGame = DatabaseGame(
+            None,
+            False,
+            [[1, 2], [2, 1]],
+            "standard",
+            1,
+            account_id,
+            None,
+            None,
+            datetime.datetime.strptime("11-01-2021 03:00:00", "%m-%d-%Y %H:%M:%S"),
+        )
+        callback.reset_mock()
+        dbm.create_game(callback=callback, database_game=dbg)
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        # Create second (more recent) game with same account as P2
+        dbg2: DatabaseGame = DatabaseGame(
+            None,
+            False,
+            [[1, 2], [2, 1]],
+            "standard",
+            1,
+            None,
+            account_id,
+            None,
+            datetime.datetime.strptime("11-02-2021 03:00:00", "%m-%d-%Y %H:%M:%S"),
+        )
+        callback.reset_mock()
+        dbm.create_game(callback=callback, database_game=dbg2)
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        # Get last game and ensure it matches second game
+        get_game_request_data = (
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+        )
+        callback.reset_mock()
+        dbm.get_game(callback, account_id, True, *get_game_request_data)
+        dbm.run()
+        game_id: int = callback.call_args[0][1].game_id
+        dbg_check = dbg2._replace(game_id=game_id)
+        callback.assert_called_once_with(True, dbg_check)
+
+        # Get second-to-last game and ensure it matches first game
+        callback.reset_mock()
+        dbm.get_game(callback, game_id - 1, False, *get_game_request_data)
+        dbm.run()
+        dbg_check = dbg._replace(game_id=game_id - 1)
+        callback.assert_called_once_with(True, dbg_check)
+
+        # Update game and check updates occurred
+        callback.reset_mock()
+        dbm.update_game(
+            callback=callback,
+            game_id=game_id,
+            database_game=DatabaseGame(complete=True, next_turn=2),
+        )
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        callback.reset_mock()
+        dbm.get_game(
+            callback=callback,
+            key=game_id,
+            last_game=False,
+            get_game_id=True,
+            get_complete=True,
+            get_next_turn=True,
+        )
+        dbm.run()
+        dbg: DatabaseGame = callback.call_args[0][1]
+        self.assertEqual(game_id, dbg.game_id)
+        self.assertEqual(True, dbg.complete)
+        self.assertEqual(2, dbg.next_turn)
+
+        # Delete games
+        callback.reset_mock()
+        dbm.delete_game(callback=callback, game_id=game_id)
+        dbm.run()
+        callback.assert_called_once_with(True)
+        callback.reset_mock()
+        dbm.delete_game(callback=callback, game_id=game_id - 1)
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        # Delete account
+        callback.reset_mock()
+        dbm.delete_account(callback=callback, account_id=account_id)
+        dbm.run()
+        callback.assert_called_once_with(True)
+
+        # Disconnect from database
+        dbm.disconnect_database()
+
+
+if __name__ == "__main__":
+    unittest.main()
