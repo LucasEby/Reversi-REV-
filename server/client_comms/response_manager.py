f7ae277f1b15d740106e47b8ab30ed866401e0df
sampontier <celloplayerone@gmail.com>
2021-11-26 16:29:04 -0500

    Update response_manager.py
    change database num top elos to 1
    
    run black formatting

--- server/client_comms/response_manager.py
+++ server/client_comms/response_manager.py
@@ -16,9 +16,15 @@ from server.client_comms.save_preferences_client_response import (
     SavePreferencesClientResponse,
 )
 from common.client_server_protocols import create_game_client_schema, get_game_client_schema
+    get_game_client_schema,
+    update_elo_client_schema,
+    get_top_elos_client_schema,
+)
 from server.client_comms.base_client_response import BaseClientResponse
 from server.client_comms.create_game_client_response import CreateGameClientResponse
 from server.client_comms.get_game_client_response import GetGameClientResponse
+from server.client_comms.get_top_elos_client_response import GetTopELOsClientResponse
+from server.client_comms.update_elo_client_response import UpdateELOClientResponse
 
 
 class ResponseManager:
