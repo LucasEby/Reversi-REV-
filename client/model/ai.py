a0cc626d9fb8545740fb48d0a48fcfadd8fd0059
LucassGithub <eby.l@northeastern.edu>
2021-12-04 11:27:50 -0500

    Added some AI stuff

--- client/model/ai.py
+++ client/model/ai.py
@@ -1,5 +1,6 @@
 from client.model.player import Player
 from client.model.user import User
+import math
 
 
 class AI(Player):
@@ -9,16 +10,56 @@ class AI(Player):
         :param player_num: Play order of AI
         """
         super().__init__(user=None, player_num=player_num)
-        self._difficulty: int = 0
+        self._difficulty = 0
 
-    @property
-    def difficulty(self) -> int:
-        return self._difficulty
-
-    @difficulty.setter
-    def difficulty(self, difficulty: int) -> None:
+    def set_difficulty(self, difficulty: int) -> None:
         """
         Set the difficulty level of the AI
         :param difficulty: Difficulty level (>0)
         """
-        self._difficulty = max(0, difficulty)
+        self._difficulty = difficulty
+        self._difficulty = max(0, self._difficulty)
+
+    def get_difficulty(self) -> int:
+        """
+        Returns the current difficulty level of the AI
+        :return: Difficulty
+        """
+        return self._difficulty
+
+    def minimax(self, cur_depth, node_index,
+                max_turn, scores):
+        """
+        Minimax algorithm that we'll use to calculate the AI decisions
+        """
+        target_depth = self._difficulty
+        # base case : targetDepth reached
+        if cur_depth == target_depth:
+            return scores[node_index]
+
+        if (max_turn):
+            return max(self.minimax(cur_depth + 1, node_index * 2,
+                               False, scores),
+                       self.minimax(cur_depth + 1, node_index * 2 + 1,
+                               False, scores))
+
+        else:
+            return min(self.minimax(cur_depth + 1, node_index * 2,
+                               True, scores),
+                       self.minimax(cur_depth + 1, node_index * 2 + 1,
+                               True, scores))
+
+
+# AI array of numbers each number will b
+#
+
+# Driver code
+scores1 = [3, 5, 2, 9, 12, 5, 23, 23]
+
+tree_depth = math.log(len(scores1), 2)
+
+ai = AI(5)
+ai.set_difficulty(int(tree_depth))
+
+print("The optimal value is : ", end="")
+print(ai.minimax(0, 0, True, scores1))
